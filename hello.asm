f71a8e6db725804381dc8c2a70c38ca92b9e8a93
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-07-31 21:38:34 +0200

    Added xmm0 and floating point number printf

--- hello.asm
+++ hello.asm
@@ -5,15 +5,24 @@
 
 global main
 extern printf
+extern puts
 
 section .data
-    message db 'Hello from asm', 0
-
+    message db 'Hello from asm', 0xA, 0 
+    format db 'passed float: %f', 0
+    v1 dd 3.1
 section .text
 
 main:
     sub rsp, 0x28                        ; Reserve the shadow space
     mov rcx, message                    ; First argument is address of message
-    call printf                            ; puts(message)
-    add rsp, 0x28                        ; Remove shadow space
+    call printf                            ; printf(message)
+
+    cvtss2sd xmm0, [v1]
+    movq rdx, xmm0
+    mov rcx, format 
+    call printf
+
+    add rsp, 0x28
+    
     ret
